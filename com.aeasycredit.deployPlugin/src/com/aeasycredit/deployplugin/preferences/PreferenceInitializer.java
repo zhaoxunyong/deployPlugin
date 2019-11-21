afc64bca24faacf5889562d781641660b0e20bb3
dave.zhao <dave.zhao@zerofinance.cn>
2019-11-21 18:40:51 +0800

    Rename git url

--- com.aeasycredit.deployPlugin/src/com/aeasycredit/deployplugin/preferences/PreferenceInitializer.java
+++ com.aeasycredit.deployPlugin/src/com/aeasycredit/deployplugin/preferences/PreferenceInitializer.java
@@ -1,5 +1,9 @@
 package com.aeasycredit.deployplugin.preferences;
 
+import java.util.MissingResourceException;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 
@@ -10,6 +14,8 @@ import com.aeasycredit.deployplugin.DeployPluginLauncherPlugin;
  */
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
+    private ResourceBundle resourceBundle;
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -17,12 +23,42 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 	 */
 	public void initializeDefaultPreferences() {
 		IPreferenceStore store = DeployPluginLauncherPlugin.getDefault().getPreferenceStore();
-		store.setDefault(PreferenceConstants.GIT_SCRIPTS_URL, "http://gitlab.aeasycredit.net/dave.zhao/deployPlugin/raw/master");
+		store.setDefault(PreferenceConstants.GIT_SCRIPTS_URL, getGitUrl());
 		store.setDefault(PreferenceConstants.GIT_SHOWTAG_IN_DROPDOWN, false);
 		store.setDefault(PreferenceConstants.GIT_RELEASE_WITH_TAG, false);
 		store.setDefault(PreferenceConstants.GIT_BASH_DEBUG, false);
 //		store.setDefault(PreferenceConstants.P_CHOICE, "choice2");
 //		store.setDefault(PreferenceConstants.P_STRING, "Defaults value");
 	}
+
+	  private String getGitUrl() {
+	  	String msg = getResourceString("git.url");
+	     return msg;
+	  }
+
+    /**
+     * Returns the string from the plugin's resource bundle,
+     * or 'key' if not found.
+     */
+  	private String getResourceString(String key) {
+        ResourceBundle bundle= getResourceBundle();
+        try {
+            return bundle.getString(key);
+        } catch (MissingResourceException e) {
+            return key;
+        }
+    }
+
+    /**
+     * Returns the plugin's resource bundle,
+     */
+  	private ResourceBundle getResourceBundle() {
+        try {
+            resourceBundle= PropertyResourceBundle.getBundle("resources");
+        } catch (MissingResourceException x) {
+            resourceBundle = null;
+        }
+        return resourceBundle;
+    }
 
 }
